dbf29d61830abeec10ca238954933bcdc5c8ae37
Shehap152 <150776777+Shehap152@users.noreply.github.com>
2023-12-09 21:16:45 +0200

    lecture model and lecture reposiotory and lecture web services

---
+++ lib/core/data/repositories/lectuer_repository.dart
@@ -0,0 +1,26 @@
+
+import '../models/lecture_model.dart';
+import '../services/lecture_web_services.dart';
+
+class LectureRepository {
+  final LectureWebServices lectureWebServices;
+
+  LectureRepository ({required this.lectureWebServices});
+
+  Future<List<Lecture>> getLectureData() async {
+    /// This is an implicit list of maps, to store our data from the web
+    /// service (Example Students).
+    final lecture =
+    await lectureWebServices.getLectureData();
+
+    /// map is a special method inside the List class that allows us to transform
+    /// each element in the list. By transforming we mean that we can change the
+    /// type of each element in the list by the lambada function.
+    return lecture
+        .map((elementInListFromWebService) =>
+        Lecture.fromJson(elementInListFromWebService))
+        .toList();
+  }
+
+}
+
